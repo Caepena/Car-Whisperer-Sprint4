0d45b150ce56d0c5465a179f19860d7ead634027
Caepena <caepenafiel@gmail.com>
2024-11-04 13:59:18 -0300

    Página de Cadastro e Atualização de Pagamentos feita.
    Co-authored-by: MarcosBisp <MarcosBisp@users.noreply.github.com>
    Co-authored-by: 7ilo <DGMMX@users.noreply.github.com>

--- car-whisperer/src/app/pagamentos/cad-pagamentos/page.tsx
+++ car-whisperer/src/app/pagamentos/cad-pagamentos/page.tsx
@@ -1,92 +1,91 @@
 "use client";
 
-import { ClienteProps } from "@/types/types";
+import { PagamentoProps } from "@/types/types";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-export default function CadClientes() {
+export default function CadPagamentos() {
     const navigate = useRouter();
 
-    const [cliente, setCliente] = useState<ClienteProps>({
-        IDCliente: 0,
-        nome: "",
-        endereco: "",
-        cpf: "",
+    const [pagamento, setPagamento] = useState<PagamentoProps>({
+        IDPagamento: 0,
+        codCliente: 0,
+        dataPagamento: "",
+        valor: 0,
     });
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = e.target;
-        setCliente({ ...cliente, [name]: value });
+        setPagamento({ ...pagamento, [name]: name === 'valor' || name === 'codCliente' ? Number(value) : value });
     };
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         try {
-            const response = await fetch("http://localhost:8080/CarWhisperer/clientes", {
+            const response = await fetch("http://localhost:8080/CarWhisperer/pagamentos", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify(cliente),
+                body: JSON.stringify(pagamento),
             });
 
             if (response.ok) {
-                alert("Cliente cadastrado com sucesso!");
-                setCliente({
-                    IDCliente: 0,
-                    nome: "",
-                    endereco: "",
-                    cpf: "",
+                alert("Pagamento cadastrado com sucesso!");
+                setPagamento({
+                    IDPagamento: 0,
+                    codCliente: 0,
+                    dataPagamento: "",
+                    valor: 0,
                 });
-                navigate.push("/clientes");
+                navigate.push("/pagamentos");
             }
         } catch (error) {
-            console.error("Falha ao cadastrar o cliente: ", error);
+            console.error("Falha ao cadastrar o pagamento: ", error);
         }
     };
 
     return (
         <div className="flex items-center justify-center mt-32">
             <div className="bg-blue-500 rounded-md p-6 w-full max-w-lg">
-                <h2 className="text-center text-2xl font-bold mb-5 text-white">Cadastro de Clientes</h2>
+                <h2 className="text-center text-2xl font-bold mb-5 text-white">Cadastro de Pagamentos</h2>
                 <form onSubmit={handleSubmit}>
                     <div className="mb-5">
-                        <label htmlFor="nome" className="block mb-2 text-sm font-medium text-white">Nome</label>
+                        <label htmlFor="codCliente" className="block mb-2 text-sm font-medium text-white">Código do Cliente</label>
                         <input
-                            type="text"
-                            id="nome"
+                            type="number"
+                            id="codCliente"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                             required
-                            name="nome"
-                            value={cliente.nome}
+                            name="codCliente"
+                            value={pagamento.codCliente}
                             onChange={handleChange}
-                            placeholder="Digite o nome do cliente"
+                            placeholder="Digite o código do cliente"
                         />
                     </div>
                     <div className="mb-5">
-                        <label htmlFor="endereco" className="block mb-2 text-sm font-medium text-white">Endereço</label>
+                        <label htmlFor="dataPagamento" className="block mb-2 text-sm font-medium text-white">Data do Pagamento</label>
                         <input
-                            type="text"
-                            id="endereco"
+                            type="date"
+                            id="dataPagamento"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                             required
-                            name="endereco"
-                            value={cliente.endereco}
+                            name="dataPagamento"
+                            value={pagamento.dataPagamento}
                             onChange={handleChange}
-                            placeholder="Digite o endereço do cliente"
                         />
                     </div>
                     <div className="mb-5">
-                        <label htmlFor="cpf" className="block mb-2 text-sm font-medium text-white">CPF</label>
+                        <label htmlFor="valor" className="block mb-2 text-sm font-medium text-white">Valor</label>
                         <input
-                            type="text"
-                            id="cpf"
+                            type="number"
+                            id="valor"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                             required
-                            name="cpf"
-                            value={cliente.cpf}
+                            name="valor"
+                            value={pagamento.valor}
                             onChange={handleChange}
-                            placeholder="Digite o CPF do cliente"
+                            placeholder="Digite o valor do pagamento"
                         />
                     </div>
 
